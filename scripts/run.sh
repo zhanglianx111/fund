3591eb889f43321e944f52ce55693efd0b6c55b5
zhanglianx111 <zhanglianx111@aliyun.com>
2020-03-04 07:34:01 +0800

    support mac and linux

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,14 +2,20 @@
 
 # 定时执行任务脚本，脚本每天执行
 # 工作内容：
-#	1. 获取当天所有基金涨跌情况
-#	2. 统计当天涨跌基金个数，并发邮件
+#	1. 获取当天所有基金涨跌情况并排序 ✅
+#	2. 统计当天涨跌基金个数，并发邮件 
 #	3. 统计当天涨幅前100名，并发邮件
 #	4. 统计当天平均涨幅，平均跌幅，并发邮件
 
 
 # 昨天日期，此脚本执行的时间是获取上一天的基金净值，所有要获取昨天的日期
-yestoday=`date -d last-day +%Y-%m-%d`
+os=`uname`
+yestoday=""
+if [ $os == "Linux" ];then
+	yestoday=`date -d last-day +%Y-%m-%d`
+elif [ $os == "Darwin" ];then
+	yestoday=`date -v-1d +"%Y-%m-%d"`
+fi
 
 python fund.py fetchall -d ${yestoday}
 if [ $? -eq 0 ];then
