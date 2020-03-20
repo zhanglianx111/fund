ac3fe06e6d1ad8eb8778437fe3dc3f5f0d52f59f
zhanglianx111 <zhanglianx111@aliyun.com>
2020-03-21 07:45:46 +0800

    yestoday param

--- scripts/routine.sh
+++ scripts/routine.sh
@@ -2,16 +2,17 @@
 
 # 昨天日期，此脚本执行的时间是获取上一天的基金净值，所有要获取昨天的日期
 os=`uname`
-yestoday=""
-if [ $os == "Linux" ];then
-	echo $os > test.log
-	echo `date` >> test.log
-	yestoday=`date -d last-day +%Y-%m-%d`
-elif [ $os == "Darwin" ];then
-	echo $os >> test.log
-	echo `date` >> test.log
-	yestoday=`date -v-1d +"%Y-%m-%d"`
+yestoday=$1
+if [ "X$yestoday" == "X" ];then
+    if [ $os == "Linux" ];then
+        echo $os > test.log
+        echo `date` >> test.log
+        yestoday=`date -d last-day +%Y-%m-%d`
+    elif [ $os == "Darwin" ];then
+        echo $os >> test.log
+        echo `date` >> test.log
+        yestoday=`date -v-1d +"%Y-%m-%d"`
+    fi
 fi
-
 
 python fund.py routine -d ${yestoday}
