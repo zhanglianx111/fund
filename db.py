2fd57440e3d33563a52967b358eafbd5c9f2bd01
LianxiangZhang <lxzhang@aibee.com>
2020-05-01 22:38:35 +0800

    bugfix

--- db.py
+++ db.py
@@ -492,11 +492,12 @@ def get_rise_by_code(fundcode, table_name, start_date, end_date):
 
 			if abs(float(format(range_totol, '.5f')) - 0.00001) <= 0.0001:
 				range_totol = float(0.0)
-
+			elif abs(float(format(range_totol, '.5f')) - 0.00001) > 0.3:
+				range_totol = -1
 		except Exception as err:
 			logger.error(err)
 
-		return (fundcode, float(format(range_totol, '.4f')), riseCount, downCount, range_max, range_min, rank_totol/length)
+		return (fundcode, float(format(range_totol, '.4f'))*100, riseCount, downCount, range_max, range_min, rank_totol/length)
 
 def get_fundname_by_code(fundcode):
 	#conn = pymysql.connect(HOST, USER, PASSWD, DB)
